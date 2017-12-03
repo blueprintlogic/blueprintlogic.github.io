9ad07f8bc6a6797498b96d2292f7e84370b04772
Scott Taylor <blueprintlogic@Scotts-MacBook-Pro.local>
2017-12-03 09:05:35 -0800

    init commit

---
+++ js/third_party.js
@@ -0,0 +1,134 @@
+
+    <!-- start Mixpanel -->
+    <script type="text/javascript" data-src="script">
+    (function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
+    for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
+    mixpanel.init("5500e860eb49f18dc64c2709762a4c0b");
+    </script>
+    <!-- end Mixpanel -->
+    
+    <!-- Start of Woopra Code -->
+    <script data-src="script" defer>
+    (function(){
+        var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call","trackForm","trackClick"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/t/5.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
+    })("woopra");
+
+    woopra.config({
+    domain: 'blueprintlogic.com'
+    });
+    woopra.track();
+    </script>
+    <!-- End of Woopra Code -->
+    
+    <!-- Start of StatCounter Code -->
+    <script defer type="text/javascript" data-src="script">
+    var sc_project=9725452; 
+    var sc_invisible=1; 
+    var sc_security="16238acc"; 
+    var scJsHost = (("https:" == document.location.protocol) ?
+    "https://secure." : "http://www.");
+    document.write("<sc"+"ript type='text/javascript' src='" +
+    scJsHost+
+    "statcounter.com/counter/counter.js'></"+"script>");
+    </script> <noscript>
+    <div class="statcounter">
+        <a href="http://statcounter.com/shopify/" target="_blank" title="shopify analytics ecommerce"><img alt="shopify analytics ecommerce" class="statcounter" src="http://c.statcounter.com/9725452/0/16238acc/1/"></a>
+    </div></noscript>
+    <!-- End of StatCounter Code -->
+  
+    <!-- ClickTale Top part -->
+    <script defer type="text/javascript" data-src="script">
+    var WRInitTime=(new Date()).getTime();
+    </script>
+    <!-- ClickTale end of Top part -->
+    
+	<!-- ClickTale Bottom part (XHTML) -->
+
+	<script defer type='text/javascript' data-src="script">
+	// The ClickTale Balkan Tracking Code may be programmatically customized using hooks:
+	// 
+	//   function ClickTalePreRecordingHook() { /* place your customized code here */  }
+	//
+	// For details about ClickTale hooks, please consult the wiki page http://wiki.clicktale.com/Article/Customizing_code_version_2
+
+	// Add this to "OnDomLoad" event
+	(function(){
+	if (typeof (ClickTaleCreateDOMElement) != "function")
+	{
+		ClickTaleCreateDOMElement = function(tagName)
+		{
+			if (document.createElementNS)
+			{
+				return document.createElementNS('http://www.w3.org/1999/xhtml', tagName);
+			}
+			return document.createElement(tagName);
+		}
+	}
+
+	if (typeof (ClickTaleAppendInHead) != "function")
+	{
+		ClickTaleAppendInHead = function(element)
+		{
+			var parent = document.getElementsByTagName('head').item(0) || document.documentElement;
+			parent.appendChild(element);
+		}
+	}
+
+	if (typeof (ClickTaleXHTMLCompliantScriptTagCreate) != "function")
+	{
+		ClickTaleXHTMLCompliantScriptTagCreate = function(code)
+		{
+			var script = ClickTaleCreateDOMElement('script');
+			script.setAttribute("type", "text/javascript");
+			script.text = code;
+			return script;
+		}
+	}
+	var scriptElement = ClickTaleCreateDOMElement('script');
+	scriptElement.type = "text/javascript";
+	scriptElement.src = (document.location.protocol=='https:'?
+	'https://cdnssl.clicktale.net/www07/ptc/a0a964e5-974c-41fe-ae17-c6e7594f1c44.js':
+	'http://cdn.clicktale.net/www07/ptc/a0a964e5-974c-41fe-ae17-c6e7594f1c44.js');
+	document.body.appendChild(scriptElement);
+	})();
+	</script>
+
+	<!-- ClickTale end of Bottom part (XHTML) -->
+    
+    <!-- goingup Tag -->
+    <script defer type='text/javascript' data-src="script">
+	document.write(unescape('%3Cscript type="text/javascript" src="'+
+	document.location.protocol+'//counter.goingup.com/js/tracker.js?ver=2&amp;st=biarg5d&amp;b=5&amp;type=javascript"%3E%3C/script%3E'));
+	</script>
+	<noscript><div><img src="//counter.goingup.com/track_biarg5d_5.php" alt="" /></div></noscript>
+	<!-- End goingup Tag -->
+
+	<!-- ClickTale Bottom part -->
+    <script defer type='text/javascript' data-src="script">
+    document.write(unescape("%3Cscript%20src='"+
+    (document.location.protocol=='https:'?
+    "https://cdnssl.clicktale.net/www07/ptc/a0a964e5-974c-41fe-ae17-c6e7594f1c44.js":
+    "http://cdn.clicktale.net/www07/ptc/a0a964e5-974c-41fe-ae17-c6e7594f1c44.js")+"'%20type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <!-- ClickTale end of Bottom part -->
+    
+    <!-- Quantcast Tag -->
+    <script defer type="text/javascript" data-src="script">
+    var _qevents = _qevents || [];
+
+    (function() {
+    var elem = document.createElement('script');
+    elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+    elem.async = true;
+    elem.type = "text/javascript";
+    var scpt = document.getElementsByTagName('script')[0];
+    scpt.parentNode.insertBefore(elem, scpt);
+    })();
+
+    _qevents.push({
+    qacct:"p-A7jpLSc7E68Mu"
+    });
+    </script> <noscript>
+    <div style="display:none;"><img alt="Quantcast" border="0" height="1" src="//pixel.quantserve.com/pixel/p-A7jpLSc7E68Mu.gif" width="1"></div></noscript>
+    <!-- End Quantcast tag -->
+    
